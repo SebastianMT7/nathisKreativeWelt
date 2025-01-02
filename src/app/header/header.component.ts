2775579c8fdd4aba1d78690ca35ad58f963fcee4
Sebastian Torney <STorney@web.de>
2025-01-02 18:39:10 +0100

    start header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  dropdownOpen: { [key: number]: boolean } = {};
 
+  openDropdown(index: number) {
+    if (this.dropdownOpen[index]) {
+      this.dropdownOpen[index] = false;
+    } else {
+      this.closeDropdowns();
+      this.dropdownOpen[index] = true;
+    }
+  }
+
+  closeDropdowns() {
+    for (let key in this.dropdownOpen) {
+      this.dropdownOpen[key] = false;
+    }
+  }
 }
