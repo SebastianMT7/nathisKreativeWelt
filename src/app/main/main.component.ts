f8165b7f12507fae982457f4aa8cdafa28bdc4b0
Sebastian Torney <STorney@web.de>
2025-01-03 18:06:50 +0100

    start single-product

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SingleProductComponent } from './single-product/single-product.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SingleProductComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
