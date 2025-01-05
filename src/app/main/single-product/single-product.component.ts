fc70d7d47d6c73edbd6c357155283d91c3bc0f06
Sebastian Torney <STorney@web.de>
2025-01-05 19:57:08 +0100

    detailcard

--- src/app/main/single-product/single-product.component.ts
+++ src/app/main/single-product/single-product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-single-product',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './single-product.component.scss'
 })
 export class SingleProductComponent {
+
+  showDetailCard = false;
+  @ViewChild('detailCard', { static: false }) detailCard!: ElementRef;
+
+
+  toggleDetailCard(){
+    this.showDetailCard = !this.showDetailCard;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleOutsideClick(event: Event):void {  
+
+    // if (this.detailCard && !this.detailCard.nativeElement.contains(event.target)) {
+    //   this.showDetailCard = false;
+    // }
+  }
 
 }
