9a08f616f75e8a74b8bc882d926e95e6efc0ddc9
Sebastian Torney <STorney@web.de>
2025-01-06 18:50:02 +0100

    products.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, MainComponent, HeaderComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
